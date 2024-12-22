fad39ab8196646ea2dfe3580730b0d64b0c368c3
Ashutosh <starcdwivedi@gmail.com>
2024-12-22 14:39:30 +0530

    add: bash to nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,44 +1,57 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = 'BufWritePre',
-        opts = require "configs.conform",
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = require "configs.conform",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "c",
+        "cpp",
+        "go",
+        "lua",
+        "python",
+        "rust",
+        "tsx",
+        "javascript",
+        "typescript",
+        "vimdoc",
+        "vim",
+        "bash",
+      },
     },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require "configs.lspconfig"
-        end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "leoluz/nvim-dap-go",
+      "nvim-neotest/nvim-nio",
     },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' }
-        },
-    },
-    {
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "rcarriga/nvim-dap-ui",
-            "leoluz/nvim-dap-go",
-            "nvim-neotest/nvim-nio",
-        },
-        config = function()
-            local dap, dapui = require "dap", require "dapui"
-            require("dap-go").setup()
-            require("dapui").setup()
-            dap.listeners.before.attach.dapui_config = function()
-                dapui.open()
-            end
-            dap.listeners.before.launch.dapui_config = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated.dapui_config = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited.dapui_config = function()
-                dapui.close()
-            end
-        end,
-    },
+    config = function()
+      local dap, dapui = require "dap", require "dapui"
+      require("dap-go").setup()
+      require("dapui").setup()
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
 }
